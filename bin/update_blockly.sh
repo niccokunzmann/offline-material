c6fb613aaf4ddacd5c6930f2749db2e1c15c5748
Coder Dojo Potsdam - ubuntu <coderdojopotsdam-discuss@googlegroups.com>
2015-09-24 12:23:36 -0700

    removed unzip bug

--- bin/update_blockly.sh
+++ bin/update_blockly.sh
@@ -15,8 +15,10 @@ mkdir -p blockly-games
 
 for zip_file in blockly-games-repo/generated/*.zip
 do
-  unzip -z $zip_file
+  echo extracting $zip_file
+  unzip $zip_file >> /dev/null
   rm -f blockly-games/index.html
 done
 
+echo copying index page
 cp -r $files_folder/blockly-games/* ../tutorials/blockly-games
