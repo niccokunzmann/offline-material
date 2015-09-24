8460018cd0e6e3655b64ab7f9292c029918ffd00
Coder Dojo Potsdam - ubuntu <coderdojopotsdam-discuss@googlegroups.com>
2015-09-24 10:47:30 -0700

    added javascript tutorials

--- bin/update_OpenTechSchool.sh
+++ bin/update_OpenTechSchool.sh
@@ -5,4 +5,10 @@ source `dirname $0`/main.sh
 mkdir -p OpenTechSchool
 cd OpenTechSchool
 
+# Python
 git_repo https://github.com/OpenTechSchool/python-beginners.git python-beginners gh-pages
+
+# Javascript
+git_repo https://github.com/OpenTechSchool/js-beginners-4h-workshop-1.git js-beginners-4h-workshop-1 gh-pages
+git_repo https://github.com/OpenTechSchool/js-beginners-day2.git js-beginners-day2 gh-pages
+
