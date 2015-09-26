1c4dfb2e7692443cc65f2506e76f34dd0b680ade
Coder Dojo Potsdam - ubuntu <coderdojopotsdam-discuss@googlegroups.com>
2015-09-25 17:27:50 -0700

    made uploading run again

--- bin/enable_uploading.sh
+++ bin/enable_uploading.sh
@@ -3,7 +3,8 @@
 chmod a+rw /var/www/offline-material/uploads/
 
 cd /var/www/offline-material
-sudo -u www-data /var/www/offline-material/bin/files/cgi_server.py 8000 1>>cgi.log 2>>cgi.log & 
-echo 'started server with pid '$!
+sudo -u www-data bash -c '/var/www/offline-material/bin/files/cgi_server.py 8000 1>>/var/log/nginx/cgi.log 2>>/var/log/nginx/cgi.log & '
 
+mkdir -p /var/www/offline-material/uploads/
+cp /var/www/offline-material/bin/files/upload.html /var/www/offline-material/uploads/
 
